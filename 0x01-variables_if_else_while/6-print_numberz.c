035dda8eebc4241c4acbb2581baa5ca78691e3db
Anthony la'O <anthonylao@live.com>
2018-01-19 00:41:15 +0000

    Betty style 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,14 @@
 *
 * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
-        int n;
-        for(n = 0; n < 10; n++)
-        {
-                putchar('n');
-        }
-	putchar('\n');
-	return(0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar ('n');
+	}
+	putchar ('\n');
+	return (0);
 }
