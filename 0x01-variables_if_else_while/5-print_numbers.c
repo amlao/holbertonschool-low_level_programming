ccefb90e10e821d6f6f8dce9ca708327cac0b14a
Anthony la'O <anthonylao@live.com>
2018-01-19 00:36:52 +0000

    Betty style 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	int n;
-	for(n = 0; n < 10; n++)
+
+	for (n = 0; n < 10; n++)
 	{
 		printf("%d\n", n);
 	}
-	return(0);
+	return (0);
 }
