db57476acd786b89e140fd9f8522a992c56fbd20
Anthony la'O <anthonylao@live.com>
2018-01-19 00:34:40 +0000

    Betty style 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,17 +6,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-        char ch;
-        for(ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-	}
-	for(ch = 'A'; ch <= 'Z'; ch++)
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);
+		putchar (ch);
 	}
-                putchar('\n');
-        return(0);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
 }
