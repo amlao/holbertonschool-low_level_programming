419a36caf638dcd136a23db4eef9d8db1b0c55b4
Anthony la'O <anthonylao@live.com>
2018-01-23 18:44:58 +0000

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - print alphabet 10 times in lowercase
  */
 void print_alphabet_x10(void)
 {
-        int x = 0;
+	int x;
 	int y;
 
-        while (; x < 11; x++)
-        {
-                _putchar('\n');
-		while (y = 'a'; y <= 'z'; y++)
+	for (x = 0; x < 11; x++)
+	{
+		_putchar('\n');
+		for (y = 'a'; y <= 'z'; y++)
 		{
 			_putchar(y);
 		}
 	}
-	return (0);
 }
