4f215635ade402e32e5e0f4f82ab582c25f976ea
Anthony la'O <anthonylao@live.com>
2018-01-24 02:55:11 +0000

    Styling

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 
-
 /**
  * print_to_98: print up to 98
  * @n: random digit
@@ -10,15 +9,15 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-	for (; n < 98; n++)
-	{
-		printf("%d, ", n);
-	}
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
 	if (n == 98)
 		printf("%d\n", n);
 	if (n > 98)
-	for (; n > 98; n--)
-	{
-		printf("%d, ", n);
-	}
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 }
