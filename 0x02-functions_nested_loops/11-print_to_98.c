e4e8a037daedf7183001e92025aa87d66aa10b31
Anthony la'O <anthonylao@live.com>
2018-01-24 02:50:37 +0000

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,15 +3,15 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
-	for (; n <= 98; n++)
+	if (n < 98)
+	for (; n < 98; n++)
 	{
 		printf("%d, ", n);
 	}
 	if (n == 98)
 		printf("%d\n", n);
-	if (n >= 98)
-	for (; n >= 98; n--)
+	if (n > 98)
+	for (; n > 98; n--)
 	{
 		printf("%d, ", n);
 	}
