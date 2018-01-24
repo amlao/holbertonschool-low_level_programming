6e50a50bae55afec9d81b0961121a42a3029857f
Anthony la'O <anthonylao@live.com>
2018-01-24 02:58:22 +0000

    Styling

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,21 @@
 void print_to_98(int n)
 {
 	if (n < 98)
+	{
 		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
+	}
 	if (n == 98)
+	{
 		printf("%d\n", n);
+	}
 	if (n > 98)
+	{
 		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
+	}
 }
