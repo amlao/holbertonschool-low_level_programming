e0f267f2ab924c10cc6191933005d50457b52893
Anthony la'O <anthonylao@live.com>
2018-01-24 01:02:59 +0000

    Syntax

---
+++ 0x02-functions_nested_loops/.#5-sign.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.11122:1516644793
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,10 @@
  */
 int print_last_digit(int x)
 {
+	x = x % 10;
+
 	if (x < 0)
-		x = x * -1;
-	_putchar(x % 10 + '0');
-	return (x % 10);
+		x = (x * -1);
+	_putchar(x + '0');
+	return (x);
 }
