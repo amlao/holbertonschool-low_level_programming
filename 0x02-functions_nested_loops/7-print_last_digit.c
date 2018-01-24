0b32c874e5e7c2bd226f3cd5ac8e59abeb14f6c8
Anthony la'O <anthonylao@live.com>
2018-01-24 00:37:11 +0000

    Fix task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,13 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n:
+ * @x: number to be tested
  * Return: the value of the last digit
  */
 int print_last_digit(int x)
 {
 	if (x < 0)
 		x = x * -1;
-	_putchar((x % 10) + '0');
-	return ((x % 10) * -1);
+	_putchar(x % 10 + '0');
+	return (x % 10);
 }
