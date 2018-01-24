9acdfc6c5504f50a56bbea43e71a256115dccb13
Anthony la'O <anthonylao@live.com>
2018-01-24 04:27:45 +0000

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,25 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
-			if (z < 10)
+			if (y == 0)
 			{
+				_putchar('0');
+			}
+			else if (z <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(z + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(z / 10 + '0');
 				_putchar(z % 10 + '0');
 			}
-			if (c == 9)
-			{
-				_putchar('\n');
-			}
+		}
+		_putchar('\n');
+	}
+}
