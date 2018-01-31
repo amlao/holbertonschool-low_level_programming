51a6e91dcc7753ec8c6d1f088485e53ed086ce58
Anthony la'O <anthonylao@live.com>
2018-01-31 01:04:52 +0000

    Set conditions

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 
 /**
  * print_array - prints an array of integers
- * @*a: array
+ * @a: array
  * @n: integer
  */
 void print_array(int *a, int n)
@@ -12,7 +12,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		printf(", ");
+		printf ("%d", a[i]);
+		if (i < n -1)
+		{
+			printf (", ");
+		}
+		else
+		{
+			printf ("\n");
+		}
 	}
 }
