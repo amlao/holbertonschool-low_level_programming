a84aa21be1f10eed286efb6522abb206decf12b7
Anthony la'O <anthonylao@live.com>
2018-02-02 03:59:30 +0000

    Betty Task 3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int counter = 0;
 
-	while (s1[counter] != '\0'&& s2[counter] != '\0')
+	while (s1[counter] != '\0' && s2[counter] != '\0')
 	{
 		if (s1[counter] != s2[counter])
 		{
