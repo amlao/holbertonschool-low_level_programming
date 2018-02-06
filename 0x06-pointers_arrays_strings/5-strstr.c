2f30ae6ae3601c4a4441b6401f9d557df4b63f71
Anthony la'O <anthonylao@live.com>
2018-02-06 02:39:54 +0000

    Rm extra files and added 6 and 8

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,12 +13,15 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; (haystack[i] != '\0') && (needle[j] != '\0');j++)
+		for (j = 0; needle[j] != '\0';j++)
 		{
-			if (needle[j] != haystack[i])
+			if (!(needle[j] == haystack[i]))
 			{
-				haystack = &haystack[i];
-				return (haystack);
+				break;
+			}
+			if (needle[j] != haystack[i]
+			{
+				
 			}
 		}
 	}
---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer
+ * @s: double pointer
+ * @to: single pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
