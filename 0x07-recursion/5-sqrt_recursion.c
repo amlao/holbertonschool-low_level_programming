0ede2b6bce26e04a6a47272ed29484db4474c204
Anthony la'O <anthonylao@live.com>
2018-02-08 22:01:20 +0000

    Betty comments

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -24,7 +24,7 @@ int root(int start, int end)
 }
 
 /**
- * sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find square root of
  * Return: -1 if function does not have a natural square root
  */
