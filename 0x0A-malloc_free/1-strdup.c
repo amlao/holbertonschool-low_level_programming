6d2eee689fc79b44ac4cbf31001192759ba00a35
Anthony la'O <anthonylao@live.com>
2018-02-13 23:27:10 +0000

    Task 1 got it to finally compile

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to copy
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int length = 0;
+	int counter = 0;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	length++;
+	s = (char *)malloc(length * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (counter = 0; counter < length; counter++)
+	{
+		s[counter] = str[counter];
+	}
+	s[counter] = '\0';
+	return (s);
+}
