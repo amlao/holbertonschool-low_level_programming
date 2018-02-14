7601876d9dabad2056b537651093c3fa493fe043
Anthony la'O <anthonylao@live.com>
2018-02-14 06:38:25 +0000

    Redid loops

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -15,41 +15,25 @@ char *str_concat(char *s1, char *s2)
 	int counter1 = 0;
 	int counter2 = 0;
 	int full;
-	char *s;
+	char *str;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	for (length1 = 0; s1[length1] != '\0'; length1++)
-	{
+	while (s1[length1] != '\0')
 		length1++;
-	}
-	length1++;
-	for (length2 = 0; s2[length2] != '\0'; length2++)
-	{
-		length2++;
-	}
-	length2++;
+	while (s2[length2++] != '\0')
+	       length2++;
 	full = length1 + length2 + 1;
-	s = (char *)malloc(sizeof(char) * full);
-	if (s == NULL)
+	str = (char *)malloc(sizeof(char) * full);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (s1[counter1] != '\0')
-	{
-		s[counter1] = s1[counter1];
-		counter1++;
-	}
-	while (s2[counter2] <= '\0')
-	{
-		s[counter1 + counter2] = s2[counter2];
-		counter2++;
-	}
-	return (s);
+	for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+		str[counter1] = s1[counter1];
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
+		str[counter2 + counter1] = s2[counter2];
+	return (str);
 }
