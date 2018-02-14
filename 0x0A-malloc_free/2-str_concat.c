a246793ff8c289153e5587143771ec0b3a9bf92f
Anthony la'O <anthonylao@live.com>
2018-02-14 07:12:53 +0000

    NULL

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -9,12 +9,12 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *str;
 	int length1 = 0;
 	int length2 = 0;
 	int counter1 = 0;
 	int counter2 = 0;
 	int full;
-	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,10 +22,8 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	for (length1 = 0; s1[length1] != '\0'; length1++)
 		;
-	length1++;
 	for (length2 = 0; s2[length2] != '\0'; length2++)
 		;
-	length2++;
 	full = length1 + length2 + 1;
 	str = (char *)malloc(sizeof(char) * full);
 	if (str == NULL)
@@ -36,5 +34,6 @@ char *str_concat(char *s1, char *s2)
 		str[counter1] = s1[counter1];
 	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 		str[counter2 + counter1] = s2[counter2];
+	str[counter2 + counter1] = '\0';
 	return (str);
 }
