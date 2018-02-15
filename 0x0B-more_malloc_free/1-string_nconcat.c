0e721c2c3c956db686092e32ea47a763572f5d8d
Anthony la'O <anthonylao@live.com>
2018-02-15 22:48:27 +0000

    Tasks 1 and 2

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: NULL if it fails, else pointer to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	{
+		char *str;
+		int length1 = 0;
+		int length2 = 0;
+		int counter1 = 0;
+		int counter2 = 0;
+		int full;
+
+		if (s1 == NULL)
+			s1 = "";
+		if (s2 == NULL)
+			s2 = "";
+		for (length1 = 0; s1[length1] != '\0'; length1++)
+			;
+		for (length2 = 0; s2[length2] != '\0'; length2++)
+			;
+		full = length1 + length2 + 1;
+		if (n < length2)
+			str = malloc(sizeof(char) * full);
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		for (counter1 = 0; s1[counter1] != '\0'; counter1++)
+			str[counter1] = s1[counter1];
+		for (counter2 = 0; s2[counter2] < n; counter2++)
+			str[counter2 + counter1] = s2[counter2];
+		str[counter2 + counter1] = '\0';
+		return (str);
+	}
+}
---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory fo ran array using malloc
+ * @nmemb: number of elements
+ * @size: number of bytes
+ * Return: NULL if malloc fails, else pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *s;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(size * nmemb);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
+	return (s);
+}
