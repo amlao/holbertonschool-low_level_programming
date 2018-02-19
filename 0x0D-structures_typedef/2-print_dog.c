77a0612069f8bd476c525d4446a81703ad4f9c29
Anthony la'O <anthonylao@live.com>
2018-02-19 23:50:40 +0000

    Restructured logic task 2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,14 +12,13 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (d->age != 0)
-		printf("Age: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
