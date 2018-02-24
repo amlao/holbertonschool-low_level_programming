39f139d99ac7bc3b2cd9187a5d2830f3ac97e503
Anthony la'O <anthonylao@live.com>
2018-02-24 22:27:17 +0000

    Tasks 2 and 3

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -20,7 +20,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(valist, char *);
 		if (s == NULL)
 			printf("(nil)");
-		if (separator == NULL)
+		else
+			printf("%s", s);
+		if (separator == NULL || counter == (n - 1))
 			printf("%s", s);
 		else
 			printf("%s", separator);
