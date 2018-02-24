01891b864b0adb2366557f3e69d5d8ade52839f4
Anthony la'O <anthonylao@live.com>
2018-02-24 21:42:35 +0000

    Fixed wrong symbols

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -7,13 +7,13 @@
  * @separator: the string to be printed between numbers
  * @n: number of integers passed
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int counter = 0;
 	va_list valist;
 
 	va_start(valist, n);
-	for (counter = 0; counter < n, counter++)
+	for (counter = 0; counter < n; counter++)
 	{
 		printf("%d", va_arg(valist, int));
 		if (separator != NULL)
