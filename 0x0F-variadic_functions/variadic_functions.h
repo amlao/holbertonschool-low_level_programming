54913dd51f400601535c7a7c9d17cdadcf9f2a5b
Anthony la'O <anthonylao@live.com>
2018-02-25 05:11:48 +0000

    Added description

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -9,10 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct type - struct of types of arguments
+ * @st: argument type
+ * @f: function pointer
+ */
+
 typedef struct type
 {
 	char *st;
-	void (*f)(va_list);
+	void (*f)(va_list valist);
 } style;
 
 #endif
