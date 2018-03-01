e599ac72afb733bd4a938974b65dc8b4d332b245
Anthony la'O <anthonylao@live.com>
2018-03-01 20:08:25 +0000

    init commit

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: linked list
+ * Return: teh number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned counter = 0;
+
+	for (; h != NULL; counter++)
+	{
+		h = h->next;
+	}
+	return (counter);
+}
