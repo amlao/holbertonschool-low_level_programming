6dad8a20549b421bce811cfc0f97f2bd37b59af4
Anthony la'O <anthonylao@live.com>
2018-03-06 18:46:43 +0000

    Got it to compile

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new;
 	listint_t *hold;
 
-	new - malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
@@ -19,7 +19,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head)
 	{
 		*head = new;
-		return (head);
+		return (new);
 	}
 	hold = *head;
 	while (hold->next)
