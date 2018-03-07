2d261d9dd461072c89c9aa71e4b11a72bcbcc7ee
Anthony la'O <anthonylao@live.com>
2018-03-07 00:59:40 +0000

    Betty comments

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * *insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of the list
- * @index: index of the node
+ * @idx: index of the node
  * @n: value of the node
  * Return: the address of the new node or NULL if it failed
  */
