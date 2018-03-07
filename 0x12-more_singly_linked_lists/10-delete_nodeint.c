5a4a4ea15718df3a65c34d4ea8e330da9604895e
Anthony la'O <anthonylao@live.com>
2018-03-07 00:58:12 +0000

    Tasks 9 and 10

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,5 +8,29 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *t, *u, *v;
+	unsigned int count = 0;
 
+	if (!head || !*head)
+		return (-1);
+	t = *head;
+	u = (*head)->next;
+	if (index == 0)
+	{
+		free(t);
+		*head = u;
+		return (1);
+	}
+	while (count < index - 1)
+	{
+		t = t->next;
+		if (!t)
+			return (-1);
+		count++;
+	}
+	v = t->next;
+	u = v->next;
+	t->next = u;
+	free(v);
+	return (1);
 }
--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,5 +9,44 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *s, *t, *u;
+	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
+
+	s = malloc(sizeof(listint_t));
+	if (s == NULL)
+		return (NULL);
+
+	s->n = n;
+	s->next = NULL;
+	t = *head;
+
+	if (!t)
+	{
+		*head = s;
+		s->next = NULL;
+		return (s);
+	}
+
+	if (idx == 0)
+	{
+		*head = s;
+		(*head)->next = t;
+		return (s);
+	}
+
+	while (i < idx - 1)
+	{
+		t = t->next;
+		if (!t)
+			return (NULL);
+		i++;
+	}
+	u = t->next;
+
+	t->next = s;
+	s->next = u;
+	return (s);
 }
