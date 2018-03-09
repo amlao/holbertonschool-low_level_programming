32da2474f5e3c04e2b800f5236fe9d27486ebe2e
Anthony la'O <anthonylao@live.com>
2018-03-09 00:49:12 +0000

    Added edge case and fixed Betty style

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -2,13 +2,15 @@
 
 /**
  * binary_to_uint - converts a binary to an unsigned int
- * @b" pointer to a string
+ * @b: pointer to a string
  * Return: the converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
 
+	if (b == NULL)
+		return (0);
 	for (; *b; b++)
 	{
 		if (*b == '1')
