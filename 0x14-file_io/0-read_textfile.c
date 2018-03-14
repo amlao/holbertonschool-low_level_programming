e7bb811e9e18e0322e7f35c23e55dc227c857e7a
Anthony la'O <anthonylao@live.com>
2018-03-14 05:31:05 +0000

    Task 0

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print else 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, fdread;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (!filename)
+		return (0);
+	if (!buffer)
+		return (0);
+	fd = open(filename, 0_RDWR);
+	if (fd == -1)
+		return (0);
+	fdread = read(fd, buffer, letters);
+	if (fdread == -1)
+		return (0);
+	write(STDOUT_FILENO, buffer, fdread);
+	return(fdread);
+}
