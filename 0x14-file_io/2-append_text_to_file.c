49e2dced561bc98f84da2c1377ee5ad6486bfe04
Anthony la'O <anthonylao@live.com>
2018-03-14 17:15:02 +0000

    Empty string

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return (1);
+		text_content = "";
 
 	fdwrite = write(fd, text_content, count);
 	if (fdwrite == -1)
