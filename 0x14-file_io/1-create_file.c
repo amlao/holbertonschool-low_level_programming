518d15134ce31b11e0e58486923d285789444956
Anthony la'O <anthonylao@live.com>
2018-03-14 06:11:44 +0000

    Task 1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the content within the file
+ * Return: 1 on success else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fdwrite;
+	int count = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR, O_CREAT, O_TRUNC, S_IRUSR, S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	while (text_content[count])
+	{
+		count++;
+	}
+	count++;
+
+	fdwrite = write(fd, text_content, counter);
+	if (fdwrite == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
