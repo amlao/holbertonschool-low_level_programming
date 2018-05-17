88ae85ddfa9d3e39ce563ebf776ca8dd5e09e371
Anthony la'O <anthonylao@live.com>
2018-05-17 21:04:35 +0000

    Moved temp->next

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -15,10 +15,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	count = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[count];
-	for (; tmp; tmp = tmp->next)
+	for (; tmp;)
 	{
 		if (strcmp(key, tmp->key) == 0)
 			return (tmp->value);
+		temp = temp->next;
 	}
 	return (NULL);
 }
