a6bc5647c7e5820bfe52f0b3bee326630a90a83a
Anthony la'O <anthonylao@live.com>
2018-05-17 21:02:51 +0000

    Check strlen key and ht->size

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 	unsigned long int count;
 
-	if (!ht || !key)
+	if (!ht || !strlen(key) || ht->size == 0)
 		return (NULL);
 	count = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[count];
