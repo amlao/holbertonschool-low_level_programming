b5ebb069977bbb8382d61932911b902ad7b9f252
Anthony la'O <anthonylao@live.com>
2018-05-17 20:51:01 +0000

    Iterate through the array

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int count;
 
 	if (size <= 0)
 		return (NULL);
@@ -15,11 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new_table)
 		return (NULL);
 	new_table->array = malloc(size * sizeof(hash_node_t *));
-	if (!new_table)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	new_table->size = size;
+	for (count = 0; count < size; count++)
+		     new_table->array[count] = NULL;
 	return (new_table);
 }
