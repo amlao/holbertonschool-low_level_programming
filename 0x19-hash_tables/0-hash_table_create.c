73099f00cf47c9318a7ef45da8c044aa81433f47
Anthony la'O <anthonylao@live.com>
2018-05-17 20:58:27 +0000

    Forgot to add back movement

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
+	new_table->size = size;
 	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
 	{
