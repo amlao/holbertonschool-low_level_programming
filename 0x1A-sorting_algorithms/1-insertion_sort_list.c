9a6ab61492aa22fd8a2276759f4ad652c71c8fec
Anthony la'O <anthonylao@live.com>
2018-05-29 22:31:54 +0000

    WIP

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,1 +1,31 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending 
+ * order
+ * @list: list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *traverse, *holder;
+
+	if (!list || !(*list) || (*list)->next == NULL)
+		return;
+
+	traverse = *list;
+	while (traverse)
+	{
+		while traverse->n < traverse->prev->n)
+		{
+			if (traverse->next != NULL)
+			{
+				traverse = traverse->prev;
+				holder->prev = traverse->prev;
+				holder->next = traverse;
+				print_list(*list);
+				holder = traverse->prev;
+			}
+		}
+		traverse = traverse->next;
+	}
+}
