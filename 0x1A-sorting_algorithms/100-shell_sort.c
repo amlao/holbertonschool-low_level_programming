184265c44aecfe4ffa74128a77fd06a7c3d17d96
Anthony la'O <anthonylao@live.com>
2018-05-31 08:38:35 +0000

    Added advance problem prototypes and updated header file

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * @array: pointer to an array
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
