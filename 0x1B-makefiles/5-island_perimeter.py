d1f745bdee7936c8f74696288b685514ff9b32ce
Anthony la'O <anthonylao@live.com>
2018-06-16 03:46:37 +0000

    Correct output

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -10,10 +10,13 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j]:
                 if i == 0 or j == 0:
                     perimeter += 1
-                if i or j == 1:
-                    if grid[i - 1][j] == 1 or grid[j][i - 1] == 1:
-                        perimeter -=2
+                if grid[i][j] == 1:
+                    perimeter += 1
+                    if i == len(grid) - 1 or j == len(grid[i]) - 1:
+                        perimeter += 1
+                    if i or j == 1:
+                        if grid[i - 1][j] == 1 or grid[j][i - 1] == 1:
+                            perimeter -= 1
     return perimeter
