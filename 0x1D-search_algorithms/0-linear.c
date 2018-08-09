012be03759f5ec1cf46c873560c647549e77d065
Anthony la'O <anthonylao@live.com>
2018-08-09 07:10:42 +0000

    Fixed first problem for checker and updated space complexity

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -15,6 +15,14 @@ int linear_search(int *array, size_t size, int value)
 	if (!array)
 		return(-1);
 	for (; i<size; i++)
+	{
 		if (array[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			return(i);
+		}
+		else
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	}
+	return (-1);
 }
