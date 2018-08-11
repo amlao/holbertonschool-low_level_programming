cb3eeedaa445126a99fd6ba1d0ed7833bfd6f09e
Anthony la'O <anthonylao@live.com>
2018-08-11 04:40:32 +0000

    Search through array

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -5,16 +5,16 @@
  * @array: the array to print
  * @size: the size of array
  */
-void print_array(int *array, size_t size)
+void print_array(int *array, size_t first, size_t last)
 {
-	size_t i = 0;
+	size_t counter;
 
-	printf("Searching in array:");
-	for (; i < size; i++)
+	counter = first;
+	for (; array != NULL && counter <= last; counter++)
 	{
-		printf(" %d", array[i]);
-		if (i != size - 1)
-			printf(",");
+		if (counter > first)
+			printf(", ");
+		printf("%d", array[counter]);
 	}
 	printf("\n");
 }
@@ -33,18 +33,21 @@ int binary_search(int *array, size_t size, int value)
 	size_t start = 0;
 	size_t mid, end;
 
+	if (array)
+		return (-1);
+
 	for (; start <= end;)
 	{
 		end = size - 1;
 		mid = (start + end) / 2;
-		print_array(array + start, end - start + 1);
+		printf("Searching in array: ");
+		print_array(array, start, end);
 		if (array[mid] == value)
 			return (mid);
 		else if (array[mid] > value)
 			end = mid - 1;
 		else
 			start = mid + 1;
-		print_array(array + start, end - start + 1);
 	}
 	return (-1);
 }
